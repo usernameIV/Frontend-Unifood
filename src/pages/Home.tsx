317d1457298c0d6acdd00ea3b5115128d5537e46
DantonF.S <141874900+usernameIV@users.noreply.github.com>
2024-10-08 03:19:15 -0300

    raw: update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 export const Home = () => {
     return (
         <div>
-            Meus ovos
+            a
         </div>
     )
 }
