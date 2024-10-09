18658da63948789e13cebda05a9859b966d5c3b5
DAN <dantonfilipe.s@gmail.com>
2024-10-08 23:24:48 -0300

    raw: add new cards to board

--- src/presentation/components/board/Board.tsx
+++ src/presentation/components/board/Board.tsx
@@ -5,8 +5,14 @@ export const Board = () => {
     return (
         <div className="main_board">
             <CardText 
-                name="Hamburguer" 
-                imageUrl="https://www.cnnbrasil.com.br/viagemegastronomia/wp-content/uploads/sites/5/2022/05/mafe-estudio-LV2p9Utbkbw-unsplash.jpg" 
+                name="Lasanha" 
+                imageUrl="https://vitarella.com.br/wp-content/uploads/2020/12/08_LASANHA_FINAL-1-min.jpg" 
+                cents=',90' 
+                value={24}
+            />
+            <CardText 
+                name="Bolo de pote" 
+                imageUrl="https://nossodocesv.lojazap.com/_core/_uploads/4680/2023/05/2335280523g80afai4d8.jpeg" 
                 cents=',90' 
                 value={24}
             />
@@ -17,14 +23,8 @@ export const Board = () => {
                 value={24}
             />
             <CardText 
-                name="Hamburguer" 
-                imageUrl="https://www.cnnbrasil.com.br/viagemegastronomia/wp-content/uploads/sites/5/2022/05/mafe-estudio-LV2p9Utbkbw-unsplash.jpg" 
-                cents=',90' 
-                value={24}
-            />
-            <CardText 
-                name="Hamburguer" 
-                imageUrl="https://www.cnnbrasil.com.br/viagemegastronomia/wp-content/uploads/sites/5/2022/05/mafe-estudio-LV2p9Utbkbw-unsplash.jpg" 
+                name="Mousse de limão" 
+                imageUrl="https://guiadacozinha.com.br/wp-content/uploads/2019/10/mousse-limao-farofa.jpg" 
                 cents=',90' 
                 value={24}
             />
